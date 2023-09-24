19e7b8bca38c9cef2031d8cd0a786ab864883784
Abdumanon11 <143066287+Abdumanon11@users.noreply.github.com>
2023-09-24 17:06:25 +0500

    Add files via upload

---
+++ dz js/index.js
@@ -0,0 +1,22 @@
+/* let arr = [[{a:{price:20}}][{a:{price:35}}][{a:{price:44}}]]
+ */
+
+/* let total = 0
+
+arr.map((num) => {
+    total = total +num
+
+}) */
+/* console.log(arr.a.price); */
+/*  */
+
+
+
+let arr = [4, 16,19,22,11,144,967,19124]
+
+let even2 = arr.filter((num) => num % 2 === 0);
+
+let even =[4,16,22,44,19124]
+let odd = [4,16,22,44,19124]
+
+ console.log(even2 , even, odd );
